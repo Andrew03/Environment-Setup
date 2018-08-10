34d22fbb7c4aada71c7db84ee26119c3e325c6ef
andrew03 <andrewhoang03@gmail.com>
2018-08-10 11:07:48 -0700

    Added in Solarized and Yank for Tmux

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ bash install_scripts/install_solarized.sh               # Adds in Solarized for 
 bash install_scripts/install_powerline.sh               # Adds in Powerline for Vim, Bash and Tmux
 bash install_scripts/install_vim_auto_pairs.sh          # Adds in Auto-Pairs for Vim (paren, brace, bracket auto-insert)
 bash install_scripts/install_vim_lasat_place.sh         # Adds in Last-Place for Vim (remember position on exit)
-bash install_scripts/install_tmux_plugin_manager.sh     # Adds in Plugin-Manager for Tmux
+bash install_scripts/install_tmux_plugins.sh            # Adds in plugins for Tmux
--- install_scripts/install_tmux_plugins.sh
+++ install_scripts/install_tmux_plugins.sh
@@ -14,6 +14,11 @@ fi
 echo "# List of Plugins" >> ~/.tmux.conf
 echo "set -g @plugin \'tmux-plugins/tpm\'" >> ~/.tmux.conf
 echo "set -g @plugin \'tmux-plugins/tmux-sensible\'" >> ~/.tmux.conf
+echo "set -g @plugin \'tmux-plugins/tmux-yank\'" >> ~/.tmux.conf
+echo "set -g @plugin \'seebi/tmux-colors-solarized\'" >> ~/.tmux.conf
+echo "" >> ~/.tmux.conf
+echo "# Enabling Solarized on Tmux" >> ~/.tmux.conf
+echo "set -g @colors-solarized \'dark\'" >> ~/.tmux.conf
 echo "" >> ~/.tmux.conf
 echo "# Intialize Tmux Plugin Manager (keep this line at the very bottom of tmux.conf)" >> ~/.tmux.conf
 echo "run source ~/.tmux.conf" >> ~/.tmux.conf
