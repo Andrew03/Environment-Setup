10aea67b946657b0cbcf7c7ce218d37eacbdc11a
andrew03 <andrewhoang03@gmail.com>
2018-08-11 15:15:09 -0700

    Updated default vimrc and added in Tmux Theme

--- install_scripts/install_tmux_plugins.sh
+++ install_scripts/install_tmux_plugins.sh
@@ -15,19 +15,25 @@ echo "# List of Plugins" >> ~/.tmux.conf
 echo "set -g @plugin 'tmux-plugins/tpm'" >> ~/.tmux.conf
 echo "set -g @plugin 'tmux-plugins/tmux-sensible'" >> ~/.tmux.conf
 echo "set -g @plugin 'tmux-plugins/tmux-yank'" >> ~/.tmux.conf
-echo "set -g @plugin 'seebi/tmux-colors-solarized'" >> ~/.tmux.conf
+echo "set -g @plugin 'jimeh/tmux-themepack'" >> ~/.tmux.conf
+# echo "set -g @plugin 'seebi/tmux-colors-solarized'" >> ~/.tmux.conf
+
+# Setting Yank Bindings Theme
 echo "" >> ~/.tmux.conf
-echo "# Enabling Solarized on Tmux" >> ~/.tmux.conf
-echo "set -g @colors-solarized 'dark'" >> ~/.tmux.conf
+echo "# Setting Yank Bindings on Tmux" >> ~/.tmux.conf
+echo "bind s copy-mode" >> ~/.tmux.conf
+echo "bind p paste-buffer" >> ~/.tmux.conf
+
+# Setting Tmux Theme
+echo "" >> ~/.tmux.conf
+echo "# Setting Theme on Tmux" >> ~/.tmux.conf
+echo "set -g @themepack 'double/cyan'" >> ~/.tmux.conf
+
+# Allowing Solarized to run on Tmux
+# echo "" >> ~/.tmux.conf
+# echo "# Enabling Solarized on Tmux" >> ~/.tmux.conf
+# echo "set -g @colors-solarized 'dark'" >> ~/.tmux.conf
+
 echo "" >> ~/.tmux.conf
 echo "# Intialize Tmux Plugin Manager (keep this line at the very bottom of tmux.conf)" >> ~/.tmux.conf
 echo "run ~/.tmux/plugins/tpm/tpm" >> ~/.tmux.conf
-
-# Allowing Solarized to run on Tmux
-if [ ! -f ~/.tmux.conf ]; then
-    echo "# For Solarized on Tmux (Added by Environment-Setup)" > ~/.bashrc
-else:
-    echo "" >> ~/.tmux.conf
-    echo "# For Solarized on Tmux (Added by Environment-Setup)" >> ~/.bashrc
-fi
-echo "alias tmux='tmux -2'" >> ~/.bashrc
